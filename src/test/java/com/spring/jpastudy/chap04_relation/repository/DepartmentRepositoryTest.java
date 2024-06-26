b0b607ccffa46e4598451a57abd2766a56e64960
hyerin11 <hodolin24@naver.com>
2024-06-26 11:43:06 +0900

    # 양방향매핑_2

--- src/test/java/com/spring/jpastudy/chap04_relation/repository/DepartmentRepositoryTest.java
+++ src/test/java/com/spring/jpastudy/chap04_relation/repository/DepartmentRepositoryTest.java
@@ -1,12 +1,15 @@
 package com.spring.jpastudy.chap04_relation.repository;
 
 import com.spring.jpastudy.chap04_relation.entity.Department;
+import com.spring.jpastudy.chap04_relation.entity.Employee;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -30,6 +33,12 @@ class DepartmentRepositoryTest {
         //then
         System.out.println("\n\n\n\n");
         System.out.println("department = " + department);
+        System.out.println("\n\n\n\n");
+
+        //모든 사원정보를 알고싶다면
+        List<Employee> employees = department.getEmployees();
+        System.out.println("\n\n\n\n");
+        employees.forEach(System.out::println);
         System.out.println("\n\n\n\n");
 
     }
