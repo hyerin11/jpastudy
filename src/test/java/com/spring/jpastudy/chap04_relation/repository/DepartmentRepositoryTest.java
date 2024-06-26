dcb99615275928ee69a20f77e5b18d32ba696360
hyerin11 <hodolin24@naver.com>
2024-06-26 12:02:12 +0900

    # 양방향_수정🌟(양방향 수정시 반대편도 같이 수정해줘야 한다)

--- src/test/java/com/spring/jpastudy/chap04_relation/repository/DepartmentRepositoryTest.java
+++ src/test/java/com/spring/jpastudy/chap04_relation/repository/DepartmentRepositoryTest.java
@@ -43,4 +43,40 @@ class DepartmentRepositoryTest {
 
     }
 
+    //양방향 연관관계에서 리스트에 데이터 갱신시 주의사항
+    @Test
+    @DisplayName("양방향 연관관계에서 연관데이터 수정")
+    void changeTest() {
+        //given
+        //3번 사원의 부서를 2번 부서에서 1번부서로 수정한다
+        // 1) 3번 사원 정보조회
+        Employee employee = employeeRepository.findById(3L).orElseThrow();
+        // 2) 1번 부서 정보조회
+        Department department = departmentRepository.findById(1L).orElseThrow();
+
+        //when
+        // 사원정보를 수정한다
+        employee.setDepartment(department);
+        //핵심!!!! 양방향에서는 수정 시 반대편에도 같이 수정해줘야 한다🌟🌟🌟
+        department.getEmployees().add(employee);
+        employeeRepository.save(employee); //다시 save하면 수정됨.
+
+
+        //then
+        //다시 2번으로 돌리고 싶다면, 2)에서 2L로 바꾸고, 바뀐부서의 사원목록을 조회한다.
+        List<Employee> employees = department.getEmployees();
+        System.out.println("\n\n\n");
+        employees.forEach(System.out::println);
+        System.out.println("\n\n\n");
+        // => 부서는 변경되었지만, 사원정보에 들어가서 보면 수정되지 않음. = 한쪽만 바꿔주면 안됨. 반대편에도 수동으로 바꿔줘야 한다.
+
+        /*
+        * 사원정보가 employee엔터티에서 수정되어도
+        * 반대편 엔터티인 department에서는 리스트에 바로 반영되지 않는다.
+        *
+        * 해결방안)
+        * 데이터 수정 시 반대편 엔터티에도 같이 수정해줘야 한다.
+        * */
+    }
+
 }
